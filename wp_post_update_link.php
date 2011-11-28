4de2d91c8648dfd4a95d73d30c59178bd13bfb74
Christian Jung <cjung@heinrich.(none)>
2011-11-29 00:07:55 +0100

    Comment out some unused code

--- wp_post_update_link.php
+++ wp_post_update_link.php
@@ -17,23 +17,22 @@ class wp_post_update_links {
         $link_html = '<ul>';
         for( $i = 1; $i <= count( $this->update_links); $i++ ){
             $link_html .= '<li>lalal</li>';
-        }
+        };
         $link_html .= '</ul>';
-        
         //return '<ul><li><a href="#">Update Link</a></li></ul>' . $content;
         return $link_html . $content;
     }
-    private function replace_update_shortcodes( $content ){
-        return $content;
-    }
+//    private function replace_update_shortcodes( $content ){
+//        return $content;
+//    }
     public function execute_update_shortcodes( $atts, $content=null, $code="" ) {
         $this->update_count++;
         $this->update_links[] = $atts['title'] ? $atts['title'] : _('Update ') . $this->update_count;
         return '<div class="update" id="update_' . $this->update_count . '">' . $content . '</div>';
     }
 };
+
 $wp_post_update_links = new wp_post_update_links();
 add_shortcode( 'update', array( $wp_post_update_links, 'execute_update_shortcodes') );
 add_filter( 'the_content', array( $wp_post_update_links, 'insert_post_update_links' ) );
-
 ?>
