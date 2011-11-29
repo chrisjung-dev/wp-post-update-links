f9e0fd6dcf5f440d6ce662fa95a80293942fcaa5
Chris Jung <campino2k@gmail.com>
2011-11-29 22:03:37 +0100

    Finalize functionality
    - add translatable content
    - add default styling via external css file
    - enqueue css file

--- wp_post_update_links.php
+++ wp_post_update_links.php
@@ -12,21 +12,26 @@ class wp_post_update_links {
 	/*
 	 * Variable Declaration
 	 */
-    private $update_count = 0;
+	private $update_count = -1;
     private $update_links = array();
     
     public function insert_post_update_links( $content ){
-        $link_html = '<ul>';
-        for( $i = 0; $i < count( $this->update_links); $i++ ){
-            $link_html .= '<li><a href="#update_' . ($i+1) .'">' . $this->update_links[ $i ] . '</a></li>';
+		global $post;
+		$link_html = '<div class="update-links-section">';
+		$link_html .= '<div class="update-links-headline">' . __( 'Updates:', 'wp_post_update_links') .'</div>';
+        $link_html .= '<ul class="update-links">';
+        foreach( $this->update_links as $i => $update_link ){
+            $link_html .= '<li><a href="#post-' . $post->ID . '_update-' . $i .'">' . $update_link . '</a></li>';
         };
         $link_html .= '</ul>';
+        $link_html .= '</div>';
         return $link_html . $content;
     }
     public function execute_update_shortcodes( $atts, $content=null, $code="" ) {
-        $this->update_count++;
-        $this->update_links[] = $atts['title'] ? $atts['title'] : _('Update ') . $this->update_count;
-        return '<div class="update" id="update_' . $this->update_count . '">' . $content . '</div>';
+		global $post;
+		$this->update_count++;
+        $this->update_links[] = $atts['title'] ? $atts['title'] : __('Update ', 'wp_post_update_links' ) . $this->update_count;
+        return '<div class="update" id="post-' . $post->ID . '_update-' . $this->update_count . '">' . $content . '</div>';
     }
 };
 $wp_post_update_links = new wp_post_update_links();
@@ -35,4 +40,5 @@ add_shortcode( 'update', array( $wp_post_update_links, 'execute_update_shortcode
  *	Add filter AFTER Shortcode to have the Update Link Array
  */
 add_filter( 'the_content', array( $wp_post_update_links, 'insert_post_update_links' ), 12 );
+wp_enqueue_style( 'wp_post_update_links_style', plugins_url( 'css/screen.css', __FILE__ ), null, 20111129, 'screen' );
 ?>
