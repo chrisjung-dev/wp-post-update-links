13c306984d014d507596a16bd2a5c549482a21c1
Chris Jung <campino2k@gmail.com>
2011-11-29 09:10:57 +0100

    Add Links zu Updates in the beginning of the article

--- wp_post_update_link.php
+++ wp_post_update_link.php
@@ -9,30 +9,30 @@
  * Author URI: http://campino2k.de
  */
 class wp_post_update_links {
-
+	/*
+	 * Variable Declaration
+	 */
     private $update_count = 0;
     private $update_links = array();
     
     public function insert_post_update_links( $content ){
         $link_html = '<ul>';
-        for( $i = 1; $i <= count( $this->update_links); $i++ ){
-            $link_html .= '<li>lalal</li>';
+        for( $i = 0; $i < count( $this->update_links); $i++ ){
+            $link_html .= '<li><a href="#update_' . ($i+1) .'">' . $this->update_links[ $i ] . '</a></li>';
         };
         $link_html .= '</ul>';
-        //return '<ul><li><a href="#">Update Link</a></li></ul>' . $content;
         return $link_html . $content;
     }
-//    private function replace_update_shortcodes( $content ){
-//        return $content;
-//    }
     public function execute_update_shortcodes( $atts, $content=null, $code="" ) {
         $this->update_count++;
         $this->update_links[] = $atts['title'] ? $atts['title'] : _('Update ') . $this->update_count;
         return '<div class="update" id="update_' . $this->update_count . '">' . $content . '</div>';
     }
 };
-
 $wp_post_update_links = new wp_post_update_links();
 add_shortcode( 'update', array( $wp_post_update_links, 'execute_update_shortcodes') );
+/*
+ *	Add filter AFTER Shortcode to have the Update Link Array
+ */
 add_filter( 'the_content', array( $wp_post_update_links, 'insert_post_update_links' ), 12 );
 ?>
